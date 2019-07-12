e6b0a789cad18a66adc6e70dfac3d331a474fa16
Robert Timms <timms@maths.ox.ac.uk>
2019-07-12 09:35:56 +0100

    add to diffusion script

--- training_workshop/spherical_diffusion.py
+++ training_workshop/spherical_diffusion.py
@@ -4,15 +4,14 @@ import pybamm
 import numpy as np
 import matplotlib.pyplot as plt
 
-pybamm.set_logging_level("DEBUG")
-
 # 1. Initialise model ------------------------------------------------------------------
 model = pybamm.BaseModel()
 
 # 2. Define parameters and variables ---------------------------------------------------
 R = pybamm.Parameter("Particle radius [m]")
 D = pybamm.Parameter("Diffusion coefficient [m2.s-1]")
-j = pybamm.Parameter("Molar ionic flux [mol.m-2.s-1]")
+j = pybamm.Parameter("Interfacial current density [A.m-2]")
+F = pybamm.Parameter("Faraday constant []")
 c0 = pybamm.Parameter("Initial concentration [mol.m-3]")
 
 c = pybamm.Variable("Concentration [mol.m-3]", domain="negative particle")
@@ -25,14 +24,18 @@ model.rhs = {c: dcdt}  # add equation to rhs dictionary
 
 # 4. State boundary conditions ---------------------------------------------------------
 lbc = pybamm.Scalar(0)
-rbc = -j / D
+rbc = -j / F / D
 model.boundary_conditions = {c: {"left": (lbc, "Dirichlet"), "right": (rbc, "Neumann")}}
 
 # 5. State initial conditions ----------------------------------------------------------
 model.initial_conditions = {c: c0}
 
 # 6. State output variables ------------------------------------------------------------
-model.variables = {"Concentration [mol.m-3]": c, "Flux [mol.m-2.s-1]": N}
+model.variables = {
+    "Concentration [mol.m-3]": c,
+    "Surface concentration [mol.m-3]": pybamm.surf(c),
+    "Flux [mol.m-2.s-1]": N,
+}
 
 "--------------------------------------------------------------------------------------"
 "Using the model"
@@ -48,8 +51,9 @@ param = pybamm.ParameterValues(
     {
         "Particle radius [m]": 10e-6,
         "Diffusion coefficient [m2.s-1]": 3.9e-14,
-        "Molar ionic flux [mol.m-2.s-1]": 6e-7,
-        "Initial concentration [mol.m-3]": 2e5,
+        "Interfacial current density [A.m-2]": 1.4,
+        "Faraday constant []": 96485,
+        "Initial concentration [mol.m-3]": 2.5e4,
     }
 )
 
@@ -60,7 +64,7 @@ param.process_geometry(geometry)
 
 # mesh and discretise
 submesh_types = {"negative particle": pybamm.Uniform1DSubMesh}
-var_pts = {r: 200}
+var_pts = {r: 20}
 mesh = pybamm.Mesh(geometry, submesh_types, var_pts)
 
 spatial_methods = {"negative particle": pybamm.FiniteVolume}
@@ -69,16 +73,21 @@ disc.process_model(model)
 
 # solve
 solver = pybamm.ScipySolver()
-t = np.linspace(0, 10, 100)
+t = np.linspace(0, 3600, 900)
 solution = solver.solve(model, t)
 
+# quick plot (won't work?)
+# output_variables = ["Concentration [mol.m-3]", "Surface concentration [mol.m-3]", "Flux [mol.m-2.s-1]"]
+# plot = pybamm.QuickPlot(model, mesh, solution, output_variables)
+# plot.dynamic_plot()
+
 # Extract output variables
-c_out = pybamm.ProcessedVariable(
-    model.variables["Concentration [mol.m-3]"], solution.t, solution.y, mesh
+c_surf = pybamm.ProcessedVariable(
+    model.variables["Surface concentration [mol.m-3]"], solution.t, solution.y, mesh
 )
 
 # plot
-plt.plot(solution.t, c_out(solution.t, r=10e-6))
+plt.plot(solution.t, c_surf(solution.t))
 plt.xlabel("Time [s]")
-plt.ylabel("Concentration [mol.m-3]")
+plt.ylabel("Surface concentration [mol.m-3]")
 plt.show()
