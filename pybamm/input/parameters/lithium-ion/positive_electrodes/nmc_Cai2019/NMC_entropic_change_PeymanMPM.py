302548ee072236605f70adca6e8f8e3a6ea89df0
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-03-08 15:52:20 -0500

    #928 get example running

--- pybamm/input/parameters/lithium-ion/positive_electrodes/nmc_Cai2019/NMC_entropic_change_PeymanMPM.py
+++ pybamm/input/parameters/lithium-ion/positive_electrodes/nmc_Cai2019/NMC_entropic_change_PeymanMPM.py
@@ -1,25 +1,24 @@
-import pybamm
+from pybamm import exp
 
 
-def NMC_entropic_change_PeymanMPM(sto, c_p_max):
+def NMC_entropic_change_PeymanMPM(sto):
     """
-        Nickel Manganese Cobalt (NMC) entropic change in open circuit potential (OCP) at
-        a temperature of 298.15K as a function of the OCP. The fit is taken from [1].
+    Nickel Manganese Cobalt (NMC) entropic change in open circuit potential (OCP) at
+    a temperature of 298.15K as a function of the OCP. The fit is taken from [1].
 
-        References
-        ----------
-        .. [1] W. Le, I. Belharouak, D. Vissers, K. Amine, "In situ thermal study of
-        li1+ x [ni1/ 3co1/ 3mn1/ 3] 1- x o2 using isothermal micro-clorimetric
-        techniques",
-        J. of the Electrochemical Society 153 (11) (2006) A2147–A2151.
+    References
+    ----------
+    .. [1] W. Le, I. Belharouak, D. Vissers, K. Amine, "In situ thermal study of
+    li1+ x [ni1/ 3co1/ 3mn1/ 3] 1- x o2 using isothermal micro-clorimetric
+    techniques",
+    J. of the Electrochemical Society 153 (11) (2006) A2147–A2151.
 
-          Parameters
-          ----------
-          sto : :class:`pybamm.Symbol`
-               Stochiometry of material (li-fraction)
+    Parameters
+    ----------
+    sto : :class:`pybamm.Symbol`
+        Stochiometry of material (li-fraction)
 
     """
-
     # Since the equation uses the OCP at each stoichiometry as input,
     # we need OCP function here
 
@@ -29,7 +28,7 @@ def NMC_entropic_change_PeymanMPM(sto, c_p_max):
         + 1.6225 * sto ** 2
         - 2.0843 * sto ** 3
         + 3.5146 * sto ** 4
-        - 0.5623 * 10 ** (-4) * pybamm.exp(109.451 * sto - 100.006)
+        - 0.5623 * 10 ** (-4) * exp(109.451 * sto - 100.006)
     )
 
     du_dT = (
