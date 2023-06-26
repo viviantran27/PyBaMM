6a281d8dab3959cdc827ee39aee9ec32821cff97
viviantran27 <viviantran27@gmail.com>
2023-06-25 20:34:39 -0400

    should be working decomp vs x

--- pybamm/models/submodels/decomposition/SEI_decomposition/SEI_decomposition.py
+++ pybamm/models/submodels/decomposition/SEI_decomposition/SEI_decomposition.py
@@ -22,7 +22,7 @@ class SeiDecomposition(pybamm.BaseSubModel):
         super().__init__(param)
 
     def get_fundamental_variables(self):
-        x_sei = pybamm.Variable("Fraction of Li in SEI", domain="current collector")
+        x_sei = pybamm.Variable("Fraction of Li in SEI", domain="negative electrode", auxiliary_domains={"secondary": "current collector"},)
 
         variables = {"Fraction of Li in SEI": x_sei}
         return variables
@@ -38,7 +38,7 @@ class SeiDecomposition(pybamm.BaseSubModel):
         r_sei_dimensional = (
             -param.therm.A_sei
             * x_sei
-            * pybamm.exp(-param.therm.E_sei / (k_b * T_av_dimensional))
+            * pybamm.exp(-param.therm.E_sei / (k_b * T_dimensional))
         )  # units 1/s
 
         m_an = rho_n_dim * param.L_y * param.L_z * param.n.L
@@ -62,4 +62,7 @@ class SeiDecomposition(pybamm.BaseSubModel):
 
     def set_initial_conditions(self, variables):
         x_sei = variables["Fraction of Li in SEI"]
-        self.initial_conditions = {x_sei: self.param.therm.x_sei_0}
+        x_sei_0 = pybamm.FullBroadcast(
+                     self.param.therm.x_sei_0, ["negative electrode"], "current collector"
+                )
+        self.initial_conditions = {x_sei: x_sei_0}
--- pybamm/models/submodels/decomposition/cathode_decomposition/no_cathode_decomposition.py
+++ pybamm/models/submodels/decomposition/cathode_decomposition/no_cathode_decomposition.py
@@ -44,5 +44,19 @@ class NoCathodeDecomposition(pybamm.BaseSubModel):
             "Outer section cathode decomposition heating": default,
             "Outer section cathode decomposition heating [W.m-3]": default,
 
+        variables = {
+            "Degree of conversion of cathode decomposition": pybamm.FullBroadcast(0, ["positive electrode"], "current collector"),
+            "Cathode decomposition reaction rate [s-1]": pybamm.FullBroadcast(0, ["positive electrode"], "current collector"),
+            "Cathode decomposition reaction rate": pybamm.FullBroadcast(0, ["positive electrode"], "current collector"),
+            "Cathode decomposition heating": pybamm.FullBroadcast(0, ["positive electrode"], "current collector"),
+            "Cathode decomposition heating [W.m-3]": pybamm.FullBroadcast(0, ["positive electrode"], "current collector"),
         }
+        # variables = {
+        #     "Degree of conversion of cathode decomposition": pybamm.Scalar(0),
+        #     "Cathode decomposition reaction rate [s-1]": pybamm.Scalar(0),
+        #     "Cathode decomposition reaction rate": pybamm.Scalar(0),
+        #     "Cathode decomposition heating": pybamm.Scalar(0),
+        #     "Cathode decomposition heating [W.m-3]": pybamm.Scalar(0),
+
+        # }
         return variables
