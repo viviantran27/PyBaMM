909447069115b698e15ed5198f3ec14ac2d450c5
Vivian <viviantran27@gmail.com>
2023-09-13 16:20:09 -0400

    export for simulink comparison. some changes to 3-state thermal.

--- pybamm/models/submodels/thermal/base_thermal.py
+++ pybamm/models/submodels/thermal/base_thermal.py
@@ -94,7 +94,7 @@ class BaseThermal(pybamm.BaseSubModel):
         Q_scale = param.i_typ * param.potential_scale / param.L_x # moved to accommodate tabbing I^2R
         I = variables["Current [A]"]
         R_tab = pybamm.Parameter("Tabbing resistance [Ohm]")
-        Q_tabbing = I**2*R_tab/ param.V_cell/Q_scale*1 # originally W.m-3
+        Q_tabbing = I**2*R_tab/ param.V_cell/Q_scale # originally W.m-3
 
         # Ohmic heating in solid
         i_s_p = variables["Positive electrode current density"]
@@ -176,7 +176,7 @@ class BaseThermal(pybamm.BaseSubModel):
             eta_r_n = variables["Negative electrode reaction overpotential"]
             Q_rxn_n = a_n * j_n * eta_r_n + Q_decomp_n
         Q_rxn_p = a_p * j_p * eta_r_p + Q_decomp_p
-        Q_rxn = pybamm.concatenation(
+        Q_rxn = pybamm.concatenation(   
             *[
                 Q_rxn_n,
                 pybamm.FullBroadcast(0, ["separator"], "current collector"),
